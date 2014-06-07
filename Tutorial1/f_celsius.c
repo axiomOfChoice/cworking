34f268e49bb06b7446bc755440e82ea5079d137e
axiomOfChoice <olinunn@gmail.com>
2014-06-07 10:58:11 +1000

    Add a header to the Celsius Fahrenheit table

--- Tutorial1/f_celsius.c
+++ Tutorial1/f_celsius.c
@@ -6,16 +6,19 @@ main(){
    int lower,upper,step;
 
    lower=0;
-   upper=100;
-   step=10;
+   upper=300;
+   step=20;
 
    fahr=lower;
 
+   printf("Fahrenheit  Celsius\n");
+   
    while (fahr<=upper){
       
       celsius=5*(fahr-32)/9;
-      printf("%d\t%d",fahr,celsius);
+      printf("%9d %8d\n",fahr,celsius);
       fahr=fahr+step;
+
    }
  
 }
