64079a7bc70a214a49634b0f02abeabc0fd49d7c
axiomOfChoice <olinunn@gmail.com>
2014-06-07 11:09:09 +1000

    Reverse order for f_celsius.c

--- Tutorial1/f_celsius_for.c
+++ Tutorial1/f_celsius_for.c
@@ -6,7 +6,7 @@ main(){
 
    printf("Fahrenheit  Celsius\n");
    
-   for (fahr=0;fahr<=300;fahr=fahr+20){
+   for (fahr=300;fahr>=0;fahr=fahr-20){
 
       celsius=5*(fahr-32)/9;
       printf("%9d %8d\n",fahr,celsius);
